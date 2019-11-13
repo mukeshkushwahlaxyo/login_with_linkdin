10fccee8538584b65cb7376bb408b3a5ec3fbddb
mukesh-kushwah <mkushwaha@laxyosolutionsoft.com>
2019-11-13 11:20:15 +0530

    completed

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('linkedin', function () {
+    return view('loginlinkedin');
+});
+Auth::routes();
+
+Route::get('/redirect', 'SocialAuthLinkedinController@redirect');
+Route::get('/', 'SocialAuthLinkedinController@callback');
+
+Route::get('/home', 'HomeController@index')->name('home');
